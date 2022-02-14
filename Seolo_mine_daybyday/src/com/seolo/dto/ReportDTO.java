0e51b5876b8c6a79e6d146b7e21b0cb1b689e0f3
leedidi <edy9527@gmail.com>
2022-02-14 23:32:09 +0900

    신고 게시판 수정

---
+++ Seolo_mine_daybyday/src/com/seolo/dto/ReportDTO.java
@@ -0,0 +1,64 @@
+package com.seolo.dto;
+
+public class ReportDTO
+{
+
+	private int rpCheck_No, check_No, ac_No, why_No;
+	private String reportDate, why;
+	
+	
+	public int getRpCheck_No()
+	{
+		return rpCheck_No;
+	}
+	public void setRpCheck_No(int rpCheck_No)
+	{
+		this.rpCheck_No = rpCheck_No;
+	}
+	public int getCheck_No()
+	{
+		return check_No;
+	}
+	public void setCheck_No(int check_No)
+	{
+		this.check_No = check_No;
+	}
+	
+	public int getAc_No()
+	{
+		return ac_No;
+	}
+	public void setAc_No(int ac_No)
+	{
+		this.ac_No = ac_No;
+	}
+	public int getWhy_No()
+	{
+		return why_No;
+	}
+	public void setWhy_No(int why_No)
+	{
+		this.why_No = why_No;
+	}
+	public String getReportDate()
+	{
+		return reportDate;
+	}
+	public void setReportDate(String reportDate)
+	{
+		this.reportDate = reportDate;
+	}
+	public String getWhy()
+	{
+		return why;
+	}
+	public void setWhy(String why)
+	{
+		this.why = why;
+	} 
+	
+
+	
+	
+	
+}
