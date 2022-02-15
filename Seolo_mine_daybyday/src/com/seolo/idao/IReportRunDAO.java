a2943b34b1df9182f833c981c62cdcdbdd2cf61b
leedidi <edy9527@gmail.com>
2022-02-15 22:51:30 +0900

    신고 게시판 수정

--- Seolo_mine_daybyday/src/com/seolo/idao/IReportRunDAO.java
+++ Seolo_mine_daybyday/src/com/seolo/idao/IReportRunDAO.java
@@ -1,10 +1,6 @@
 package com.seolo.idao;
 
-import java.util.ArrayList;
-
 import com.seolo.dto.ReportRunDTO;
-import com.seolo.dto.ReportviewDTO;
-import com.seolo.personal.PersonalDTO;
 
 public interface IReportRunDAO
 {
--- Seolo_mine_daybyday/src/com/seolo/idao/IReportviewDAO.java
+++ Seolo_mine_daybyday/src/com/seolo/idao/IReportviewDAO.java
@@ -2,23 +2,34 @@ package com.seolo.idao;
 
 import java.util.ArrayList;
 
-import com.seolo.dto.ReportRunDTO;
+import org.apache.ibatis.annotations.Param;
+
 import com.seolo.dto.ReportviewDTO;
 
 public interface IReportviewDAO
 {
-	// 신고 게시판 조회
-	public ArrayList<ReportviewDTO> list();
+	// 신고 게시판: 전체 조회
+	public ArrayList<ReportviewDTO> list(@Param("start") int start, @Param("end") int end);
+
+	// 신고 게시판: 카테고리별 조회
+	public ArrayList<ReportviewDTO> cateList(@Param("start") int start, @Param("end") int end, @Param("report_check") String report_check);
+
+	// 신고 게시판: 전체 게시글 갯수
+	public int count();
+	
+	// 신고 게시판: 카테고리별 게시글 갯수
+	public int cateCount(String report_check);
 	
 	// 신고 게시글 조회
 	public ReportviewDTO view(int num);
 
-	// 신고 게시판 - 카테고리별 조회
-	public ArrayList<ReportviewDTO> cateList(String report_check);
+	// 마이페이지: 나의 신고리스트(전체) 조회
+	public ArrayList<ReportviewDTO> myinfoAllList(@Param("start") int start, @Param("end") int end, @Param("reportername") String reportername);
 	
+	// 마이페이지: 각 유저의 신고게시글 갯수 조회
+	public int myCount(String reportername);		
+
 	// 신고 게시판 - 카테고리 조회
 	public ArrayList<ReportviewDTO> cateNameList();
-	
-	// 마이페이지 - 나의 신고리스트(전체) 조회
-	public ArrayList<ReportviewDTO> myinfoAllList(String reportername);
+
 }
